5f23fba6605d810d2da3f455e48a7b7aedbd960a
apromac <abraham.tiene@apromac.ci>
2022-08-27 02:29:40 +0000

    Mise à jour de l'implementation du microservice saigneur-gateway

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# ======================================================================
+# DOCKERFILE
+# CONSTRUCTION DE L'IMAGE DOCKER DU MICROSERVICE "saigneur-gateway"
+# ======================================================================
+FROM openjdk:11-alpine
+LABEL maintainer = "apromac <abraham.tiene@apromac.ci>"
+
+RUN mkdir /usr/local/microservice
+WORKDIR /usr/local/microservice
+COPY target/*.jar saigneur-gateway.jar
+
+EXPOSE 9001
+ENTRYPOINT ["java", "-jar", "saigneur-gateway.jar"]
---
+++ src/main/java/com/apromac/saigneur/SaigneurGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.apromac.saigneur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class SaigneurGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaigneurGatewayApplication.class, args);
+	}
+
+}
