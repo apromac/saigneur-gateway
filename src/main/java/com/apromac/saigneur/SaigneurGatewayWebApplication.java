811d20ddf307f3adb55da4b4fbe58cdd6679db03
apromac <abraham.tiene@apromac.ci>
2022-10-04 14:37:55 +0000

    update files

--- src/main/java/com/apromac/saigneur/SaigneurGatewayWebApplication.java
+++ src/main/java/com/apromac/saigneur/SaigneurGatewayWebApplication.java
@@ -3,11 +3,14 @@ package com.apromac.saigneur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
 //@EnableZuulProxy
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+
+@SpringBootApplication
+@EnableEurekaClient
 public class SaigneurGatewayWebApplication {
 
 	public static void main(String[] args) {
